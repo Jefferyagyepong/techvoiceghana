47d8207fdfd56ebebbf338313a9b87fc656580da
Jefferyagyepong <agyepong9jeff@icloud.com>
2024-02-16 03:34:23 +0000

    go

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,12 @@
 export default function Footer() {
   return (
     <footer>
-      <h5>Subscibe to Our Newsletter 📃</h5>
+      <h5>Subscibe to Our Newsletter 📃</h5><br/>
           <form>
               <label for="email">Email</label>
               <input type="email" name="email" />
               <button type="submit">submit</button>
-          </form>
+          </form><br/>
     
 
       <h6>All rights Reserved &copy; TechVoice 🇬🇭 {new Date().getFullYear()}</h6>
