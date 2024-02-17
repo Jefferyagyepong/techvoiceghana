852c0c76e867592d29d4d66c55e66199097619b7
Jefferyagyepong <agyepong9jeff@icloud.com>
2024-02-17 00:32:46 +0000

    go

--- components/Post.js
+++ components/Post.js
@@ -5,7 +5,7 @@ const Post = ({ post: { title, body, imgUrl, author }, index }) => {
     return (
         <div className={styles.post}>
             <h1 className={styles.heading}>{title}</h1>
-            <Image className={styles.image} src={imgUrl} alt="post" />
+            <Image className={styles.image} src={imgUrl} alt="post" width={100} height={120}/>
             <p>{body}</p>
             <div className={styles.info}>
                 <h4> by: {author}</h4>
